157f308e058c49edab3dd6bcc8f150696671dc81
d3vv3 <jaimecondel@gmail.com>
2024-03-02 14:21:46 +0100

    feat(api): fetch and response instead of forward

--- backend/src/routers/feed.py
+++ backend/src/routers/feed.py
@@ -2,7 +2,6 @@ from os import getenv
 
 import requests
 from fastapi import APIRouter, Response
-from fastapi.responses import RedirectResponse
 from src.models.user import User
 
 API_BASE_URL = getenv("API_BASE_URL", "https://rssfilter.sgn.space/api/v1")
@@ -20,13 +19,5 @@ async def save_feed(user_id, feed_url):
     user = User(uid=user_id)
     # TODO: Do something with user
 
-    feed = requests.get(feed_url)
-    feed_xml = feed.text
-    return Response(content=feed_xml, media_type="application/xml")
-
-    # feed = feedparser.parse(feed_url)
-    # TODO: Do something with the feed (create article, embeddings...)
-    # if feed.status != 200:
-    #     return RedirectResponse(feed_url)
-    # return feed
-    return RedirectResponse(feed_url)
+    feed_response = requests.get(feed_url)
+    return Response(content=feed_response.text, media_type="application/xml")
