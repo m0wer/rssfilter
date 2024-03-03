2aacb60a03854cf1ecc876e2db5a42ddf1155b12
d3vv3 <jaimecondel@gmail.com>
2024-03-03 11:15:33 +0100

    fix(backend): no namespace and compatibility with hackernews

--- backend/src/routers/feed.py
+++ backend/src/routers/feed.py
@@ -1,19 +1,14 @@
-import xml.etree.ElementTree as ET
 from datetime import datetime, timezone
 from os import getenv
 
 import requests
 from fastapi import APIRouter, Response
-from loguru import logger
 from sqlmodel import Session, select
 from src.db import engine
 from src.models.feed import Feed
 from src.models.user import User
+from src.modules.rss import RSS
 
-from sqlmodel import Session, select
-
-
-from src.db import engine
 API_BASE_URL = getenv("API_BASE_URL", "https://rssfilter.sgn.space/").rstrip("/")
 ROOT_PATH = getenv("ROOT_PATH", "/api").lstrip("/")
 
@@ -47,14 +42,6 @@ async def save_feed(user_id, feed_url):
         session.commit()
 
     feed_response = requests.get(feed_url)
-    feed_root = ET.fromstring(feed_response.text)
-    namespace = feed_root.tag.split("}")[0][1:]
-    for link in feed_root.findall(".//{%s}link" % namespace):
-        old_link = link.get("href")
-        logger.debug("OLD LINK: {link}", link=old_link)
-        new_link = f"{API_BASE_URL}/{ROOT_PATH}/v1/log/{user_id}/{old_link}"
-        logger.debug("NEW LINK: {link}", link=new_link)
-        link.set("href", new_link)
-
-    modified_feed = ET.tostring(feed_root, encoding="unicode")
+    feed = RSS(feed_response.text, user_id)
+    modified_feed = feed.get_modified_rss()
     return Response(content=modified_feed, media_type="application/xml")
