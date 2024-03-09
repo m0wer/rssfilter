2a885cab52d106d938b3cd2ae63abb093257bcb6
m0wer <m0wer@autistici.org>
2024-03-09 08:53:23 +0100

    add timing and gzip middlewares

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,9 +1,12 @@
 from os import getenv
+import time
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.gzip import GZipMiddleware
 from sqlmodel import SQLModel
 from src.routers.common import get_engine
 from src.routers import feed, log
+from loguru import logger
 
 from contextlib import asynccontextmanager
 
@@ -22,3 +25,16 @@ app.include_router(feed.router)
 app.include_router(feed.router, prefix="/v1")
 app.include_router(log.router, prefix="/v1")
 app.include_router(feed.router, prefix="/latest")
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    logger.debug(f"Processed request in {process_time:.3f} seconds")
+    return response
+
+
+app.add_middleware(GZipMiddleware, minimum_size=1000)
