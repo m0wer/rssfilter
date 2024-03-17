9df5ba2c355a320c1d871c8d8f0fcb21b887ed88
m0wer <m0wer@autistici.org>
2024-03-17 17:18:25 +0100

    add recommendation test and useful commands docs

--- backend/tests/test_recommend.py
+++ backend/tests/test_recommend.py
@@ -1,27 +1,46 @@
-from os import getenv
-import pytest
-import random
-
 from app.recommend import filter_articles
-from sqlmodel import create_engine, Session, select
 
 
 from app.models.article import Article
-from app.models.user import User
 
 
-@pytest.mark.skip(reason="WIP")
 class TestRecommend:
-    def get_engine(self):
-        return create_engine(getenv("DATABASE_URL", "sqlite:////tmp/db.sqlite"))
-
-    def test_filter_articles(self):
-        with Session(self.get_engine()) as session:
-            user = session.exec(
-                select(User).where(User.id == "4f3e1d44738d433c9466deda724d0ad1")
-            ).first()
-            read_articles = user.articles
-            # get 100 random articles
-            random_articles = random.sample(session.exec(select(Article)).all(), 100)
-        filtered_articles = filter_articles(read_articles, random_articles)
-        breakpoint()
+    def test_filter_articles_dummy_data(self):
+        read_articles = [
+            Article(
+                id=1,
+                title="Everything about apples",
+                description="Apples are a great fruit, healthy and tasty",
+            ),
+            Article(
+                id=2,
+                title="The best way to eat apples",
+                description="Apples are great, but how to eat them?",
+            ),
+            Article(
+                id=3,
+                title="Receta de compota de manzana",
+                description="Fácil receta para hace en casa tu propia compota de manzana",
+            ),
+        ]
+        articles_to_filter = [
+            Article(
+                id=4,
+                title="Apple crumble recipe",
+                description="Easy recipe to make apple crumble at home",
+            ),
+            Article(
+                id=5,
+                title="Estoicism",
+                description="Stay calm and carry on",
+            ),
+        ]
+        filtered_articles = filter_articles(
+            articles=articles_to_filter,
+            read_articles=read_articles,
+            filter_ratio=0.5,
+            random_ratio=0,
+            n_clusters=1,
+        )
+        assert len(filtered_articles) == 1
+        assert filtered_articles[0].id == 4
