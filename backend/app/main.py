2b65e7e6dab49ef681d62e8dffb6890f7557c6c9
m0wer <m0wer@autistici.org>
2024-03-23 10:52:37 +0100

    log time in ms

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,7 +39,7 @@ async def add_process_time_header(request: Request, call_next):
     response = await call_next(request)
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
-    logger.debug(f"Processed request in {process_time:.3f} seconds")
+    logger.debug(f"Processed request in {round(process_time*1000)} ms")
     return response
 
 
