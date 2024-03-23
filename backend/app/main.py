4c92bb4883a164205437c9c4bec17d66c1535e82
m0wer <m0wer@autistici.org>
2024-03-23 08:12:27 +0100

    fix contants

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.gzip import GZipMiddleware
 from sqlmodel import SQLModel
 from app.routers.common import get_engine
 from app.routers import feed, log
+from app.constants import ROOT_PATH
 from loguru import logger
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
@@ -13,8 +14,6 @@ from fastapi_cache.backends.inmemory import InMemoryBackend
 from redis import asyncio as aioredis  # type: ignore
 
 from contextlib import asynccontextmanager
-
-ROOT_PATH = getenv("ROOT_PATH", "")
 
 
 @asynccontextmanager
