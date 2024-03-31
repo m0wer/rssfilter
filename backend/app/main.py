87a044862ee419758dd224599d62d46b111ef2ff
m0wer <m0wer@autistici.org>
2024-03-31 21:19:42 +0200

    add CORS headers to backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ import time
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import feed, log, signup, user
 from app.constants import ROOT_PATH
 from loguru import logger
@@ -46,3 +47,10 @@ async def add_process_time_header(request: Request, call_next):
 
 
 app.add_middleware(GZipMiddleware, minimum_size=1000)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
